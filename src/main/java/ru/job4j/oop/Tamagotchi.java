47ea68e03afd6660f3e35f3aee3bcea9495cf807
Konstantin Karpov <karpovkottt@gmail.com>
2023-09-26 16:17:54 +1000

    Переменные и ссылочные типы данных [#505100 #447851]

---
+++ src/main/java/ru/job4j/oop/Tamagotchi.java
@@ -0,0 +1,25 @@
+package ru.job4j.oop;
+
+public class Tamagotchi {
+
+    private int weight = 100;
+
+    public void feed() {
+        weight += 10;
+    }
+
+    public String info() {
+        return "Weight: " + weight;
+    }
+
+    public static void feedPet(Tamagotchi petCopy) {
+        petCopy.feed();
+    }
+
+    public static void main(String[] args) {
+        Tamagotchi pet = new Tamagotchi();
+        System.out.println("Weight: " + pet.info());
+        feedPet(pet);
+        System.out.println("Weight: " + pet.info());
+    }
+}
