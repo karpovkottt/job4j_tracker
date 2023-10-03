deb23c7a4ccacb897ed019d7fc2a186a63a5559d
Konstantin Karpov <karpovkottt@gmail.com>
2023-10-03 17:46:42 +1000

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,22 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[size];
+        int sizeArrCopy = 0;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (key.equals(item.getName())) {
+                rsl[index] = item;
+                sizeArrCopy++;
+            }
+        }
+        return Arrays.copyOf(rsl, sizeArrCopy);
     }
 }
