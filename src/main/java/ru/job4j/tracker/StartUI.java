9a99f1da3ab3fb578edb8812e2c2ef9971d1f85c
Konstantin Karpov <karpovkottt@gmail.com>
2023-10-04 16:41:05 +1000

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -8,5 +8,6 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String currentDateTime = item.getLocalDateTime().format(formatter);
         System.out.println(currentDateTime);
+        System.out.println(item);
     }
 }
