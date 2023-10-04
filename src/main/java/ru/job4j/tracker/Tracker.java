389daf699c1339b2078c12af4b9c2be9ddce4f7b
Konstantin Karpov <karpovkottt@gmail.com>
2023-10-04 17:33:41 +1000

    fix: 7. Метод удаления заявки Tracker.delete [#211749 #449766]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -60,9 +60,7 @@ public class Tracker {
         int index = indexOf(id);
         if (index != -1) {
             items[index] = null;
-            int start = index + 1;
-            int length = size - index - 1;
-            System.arraycopy(items, start, items, index, length);
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
         }
