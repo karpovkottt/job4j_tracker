195e692c744f6e2218b3f8e7b9b0f194e09c2b35
Konstantin Karpov <karpovkottt@gmail.com>
2023-10-06 15:45:53 +1000

    15. Аргументы переменной длины (varargs) [#504977]

---
+++ src/main/java/ru/job4j/varargs/VarArgs.java
@@ -0,0 +1,19 @@
+package ru.job4j.varargs;
+
+public class VarArgs {
+    public static String text(String... strings) {
+        StringBuilder builder = new StringBuilder();
+        for (String s: strings) {
+            builder.append(s);
+        }
+        return builder + " Количество соединенных аргументов: " + strings.length;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(text());
+        System.out.println(text("aaa"));
+        System.out.println(text("aaa", "bbb"));
+        System.out.println(text("aaa", "bbb", "ccc"));
+        System.out.println(text("aaa", "bbb", "ccc", "ddd"));
+    }
+}
