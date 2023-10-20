4934d872dda2c10f8ff2cff46a3b30cee529f571
Konstantin Karpov <karpovkottt@gmail.com>
2023-10-20 18:18:55 +1000

    0.5. Иерархия исключений и множественный catch. [#219368]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,10 +1,10 @@
 package ru.job4j.ex;
 
 public class FindEl {
-    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+    public static int indexOf(String[] values, String key) throws ElementNotFoundException {
         int rls = -1;
-        for (int i = 0; i < value.length; i++) {
-            if (value[i].equals(key)) {
+        for (int i = 0; i < values.length; i++) {
+            if (values[i].equals(key)) {
                 rls = i;
                 break;
             }
@@ -13,6 +13,25 @@ public class FindEl {
             throw new ElementNotFoundException("Element not found");
         }
         return rls;
+    }
+
+    public static boolean sent(String value, String[] abuses) throws ElementAbuseException {
+        for (String val : abuses) {
+            if (value.equals(val)) {
+                throw new ElementAbuseException("Value not allowed");
+            }
+        }
+        return true;
+    }
+
+    public static void process(String[] values, String key, String[] abuses) {
+        try {
+            if (indexOf(values, key) != -1) {
+                sent(key, abuses);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,46 @@
+package ru.job4j.ex;
+
+public class UserStore {
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        User user = null;
+        for (User us : users) {
+            if (login.equals(us.getUsername())) {
+                user = us;
+                break;
+            }
+        }
+        if (user == null) {
+            throw new UserNotFoundException("User not found");
+        }
+        return user;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (!user.isValid() || user.getUsername().length() < 3) {
+            throw new UserInvalidException("Invalid User");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Konst Karpov", true)
+        };
+        try {
+            User user = findUser(users, "Konst Karpov");
+            if (user != null) {
+                if (validate(user)) {
+                    System.out.println("This user has an access");
+                }
+            }
+        } catch (UserInvalidException ei) {
+            ei.printStackTrace();
+        } catch (UserNotFoundException en) {
+            en.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } catch (Throwable th) {
+            th.printStackTrace();
+        }
+    }
+}
