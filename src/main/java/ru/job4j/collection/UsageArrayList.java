9dbc2296a0a62dd59ceaf00d6bb8600040e0e652
Konstantin Karpov <karpovkottt@gmail.com>
2023-11-07 18:10:02 +1000

    fix: 2. Коллекции, ArrayList, List, Обобщения [#10094 #458346]

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -8,7 +8,7 @@ public class UsageArrayList {
         names.add("Ivan");
         names.add("Petr");
         names.add("Stephan");
-        for (Object value : names) {
+        for (String value : names) {
             System.out.println(value);
         }
     }
