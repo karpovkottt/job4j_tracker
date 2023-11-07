4f242060fe329a634baa63141293a8df11379c81
Konstantin Karpov <karpovkottt@gmail.com>
2023-11-07 14:54:53 +1000

    2. Коллекции, ArrayList, List, Обобщения [#10094]

---
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+
+public class UsageArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Ivan");
+        names.add("Petr");
+        names.add("Stephan");
+        for (Object value : names) {
+            System.out.println(value);
+        }
+    }
+}
