14936b7043b57590ae68918379b0a4c626304b46
Konstantin Karpov <karpovkottt@gmail.com>
2023-11-09 15:48:44 +1000

    fix: 9. Изменить программу Tracker из 2-го модуля [#10039 #458629]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -19,7 +19,7 @@ public class Tracker {
     }
 
     public List<Item> findAll() {
-        return items;
+        return List.copyOf(items);
     }
 
     public List<Item> findByName(String key) {
@@ -34,12 +34,10 @@ public class Tracker {
 
     private int indexOf(int id) {
         int rsl = -1;
-        int index = 0;
-        for (Item item : items) {
-            if (item.getId() == id) {
+        for (int index = 0; index < items.size(); index++) {
+            if (items.get(index).getId() == id) {
                 rsl = index;
             }
-            index++;
         }
         return rsl;
     }
