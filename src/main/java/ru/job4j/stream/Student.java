afcea4e2c9cee9340f59b0b645352f1fc585a72d
Konstantin Karpov <karpovkottt@gmail.com>
2023-12-06 19:16:50 +1000

    5. Фильтрация учеников [#110058]

---
+++ src/main/java/ru/job4j/stream/Student.java
@@ -0,0 +1,38 @@
+package ru.job4j.stream;
+
+import java.util.Objects;
+
+public class Student {
+    private int score;
+    private String surname;
+
+    public Student(int score, String surname) {
+        this.score = score;
+        this.surname = surname;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        Student student = (Student) object;
+        return score == student.score && Objects.equals(surname, student.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(score, surname);
+    }
+}
