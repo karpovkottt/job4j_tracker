cc32491b6fff337d418e917f0c6859591ed2b996
Konstantin Karpov <karpovkottt@gmail.com>
2023-12-06 17:41:17 +1000

    3. Отладка лямбда в IDEA [#259165]

--- src/main/java/ru/job4j/stream/StreamUsage.java
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -1,8 +1,6 @@
 package ru.job4j.stream;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class StreamUsage {
     public static class Task {
@@ -13,68 +11,18 @@ public class StreamUsage {
             this.name = name;
             this.spent = spent;
         }
-
-        @Override
-        public String toString() {
-            return "Task{"
-                    + "name='" + name + '\''
-                    + "spent=" + spent
-                    + '}';
-        }
     }
 
     public static void main(String[] args) {
         List<Task> tasks = List.of(
-                new Task("Bug #1", 100),
-                new Task("Task #2", 100),
-                new Task("Bug #3", 100)
+                new Task("Bug #1", 10),
+                new Task("Task #2", 20),
+                new Task("Bug #3", 40)
         );
-
-        System.out.println("Фильтрация-->с помощью Stream<--");
-        List<Task> bugs = tasks.stream().filter(
-                task -> task.name.contains("Bug")
-        ).collect(Collectors.toList());
-        bugs.forEach(System.out::println);
-
-        System.out.println("\n-->тоже с помощью цикла<--");
-        List<Task> container = new ArrayList<>();
-        for (Task task : tasks) {
-            if (task.name.contains("Bug")) {
-                container.add(task);
-            }
-        }
-        container.forEach(System.out::println);
-
-        System.out.println("\nПреобразование-->с помощью Stream<--");
-        List<String> names = tasks.stream().map(
-                task -> task.name
-        ).collect(Collectors.toList());
-        names.forEach(System.out::println);
-
-        System.out.println("\n-->тоже с помощью цикла<--");
-        List<String> container2 = new ArrayList<>();
-        for (Task task : tasks) {
-            container2.add(task.name);
-        }
-        container2.forEach(System.out::println);
-
-        System.out.println("\nУпрощение-->с помощью Stream<--");
-        long total = tasks.stream().map(
-                task -> task.spent
-        ).reduce(0L, Long::sum);
-        System.out.println(total);
-
-        System.out.println("\n-->тоже с помощью цикла<--");
-        total = 0L;
-        for (Task task : tasks) {
-            total += task.spent;
-        }
-        System.out.println(total);
-
-        System.out.println("\nАккумуляция-->с помощью Stream<--");
-        List<Task> bugs2 = tasks.stream().filter(
-                task -> task.name.contains("Bug")
-        ).collect(Collectors.toList());
-        bugs2.forEach(System.out::println);
+        tasks.stream()
+                .filter(task -> task.name.contains("Bug"))
+                .filter(task -> task.spent > 30)
+                .map(task -> task.name + " " + task.spent)
+                .forEach(System.out::println);
     }
 }
