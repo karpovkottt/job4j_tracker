3d7aec29ffa22ec0ad0f0813159d14c406cae144
Konstantin Karpov <karpovkottt@gmail.com>
2023-12-07 12:40:34 +1000

    6. Список адресов [#110062]

---
+++ src/main/java/ru/job4j/stream/Address.java
@@ -0,0 +1,34 @@
+package ru.job4j.stream;
+
+import java.util.Objects;
+
+public class Address {
+    private String city;
+    private String street;
+    private int home;
+    private int apartment;
+
+    public Address(String city, String street, int home, int apartment) {
+        this.city = city;
+        this.street = street;
+        this.home = home;
+        this.apartment = apartment;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        Address address = (Address) object;
+        return home == address.home && apartment == address.apartment && Objects.equals(city, address.city) && Objects.equals(street, address.street);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, home, apartment);
+    }
+}
