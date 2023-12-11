eb0f7448cfd39d99af36606a7bf4f9ac0470b611
Konstantin Karpov <karpovkottt@gmail.com>
2023-12-11 16:54:37 +1000

    13. Методы преобразования потоков объектов в потоки примитивы и наоборот [#504927]

---
+++ src/main/java/ru/job4j/stream/mapto/Person.java
@@ -0,0 +1,54 @@
+package ru.job4j.stream.mapto;
+
+import java.util.Objects;
+
+public class Person {
+    private String name;
+    private Integer age;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        Person person = (Person) object;
+        return Objects.equals(name, person.name) && Objects.equals(age, person.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{"
+                + "name='" + name + '\''
+                + "age=" + age
+                + '}';
+    }
+}
